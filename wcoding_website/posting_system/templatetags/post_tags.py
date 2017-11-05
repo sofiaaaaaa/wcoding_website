4511c47b6b1a1c57c70478bd42fe479b269dfea6
Nick <ultrasound2k@gmail.com>
2017-11-05 16:39:24 +0900

    modified list page

--- wcoding_website/posting_system/templatetags/post_tags.py
+++ wcoding_website/posting_system/templatetags/post_tags.py
@@ -7,6 +7,14 @@ from ..models import Post
 
 register = template.Library()
 
+
+@register.assignment_tag
+def show_all_programs():
+    posts = Post.published.all()
+
+    return posts.exclude(category='news_events').exclude(category='meet_the_team').exclude(category='main_window')
+
+
 @register.assignment_tag
 def show_fulltime_course(count=1):
     posts = Post.published.all()
@@ -65,17 +73,12 @@ def show_past_news_events():
 
 @register.simple_tag
 def total_posts():
+
     return Post.published.count()
 
 
 @register.inclusion_tag('wcoding/post/latest_posts.html')
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
+
     return {'latest_posts': latest_posts}
-
-
-# @register.assignment_tag
-# def get_most_commented_posts(count=5):
-#     return Post.published.annotate(
-#         total_comments=Count('comments')
-#     ).order_by('-total_comments')[:count]
